b6b801d9f55407e16edfb73d1e003a7db3f84207
igor <igorvpsilva06@gmail.com>
2024-09-26 12:00:04 -0300

    janela2

--- janela2.py
+++ janela2.py
@@ -16,23 +16,50 @@ import sys
 # um título para a janela 
 
 class janela2(QWidget):
+    # o comando def(definition->definição) define uma fnção
+    # neste caso estamos definindo a função de inicialização
     
     def __init__(self):
          super().__init__()
     #adicionar um texto ao titulo da janela
     self.setWindowTiltle("janela de casdastro")
+    
     #configurar o tamaho e posição
+    # o pirmeiro valor é a posçao x medida em pixel
+    # o segundo valor é a posição em y medidaa em pixel
+    # o terceiro valor é a largura (width) medida em pixel
+    # o quarto valor é a altura(height) medida em pixel
+    
     self.setGeometry(50,200,500,200)
-
-    #adicionar uma label a janela
+     # uma label(rótulo) é um texto que é apresentado
+     # em uma janela para indicar o que deve ser feito
+     # frente(geralmente uma caixa de texto). no exemplo abaixo
+     # estamos criando uma label para apresentarmos o texto
+     # nome completo, indica o que o usuario deve escrever
+     # o nome de uma caixa de texto a frente 
+     # geralmete, uma label é usada em uma combinação de caixa
+     # de texto (Qlineedit)
+     # adicionar uma label a janela 
+     
     self.label_nome = Qlabel("nome completo: ")
 
     #adicionar uma caixa de texto
-    self.edit_nome = QlineEdit()
-
+    self.edit_nome = QlineEdit() 
+    
+    # estamos usando o gerenciador de layout vertical (QVboxLayout)
+    # ele é ultilizado para organizar os controles que irão aparecer
+    # na janela2. O QVBoxLayout foiu ultilizado para dipor os 
+    # os controles um abaxio do outro. Para exibir um ao lado do outro
+    # usamos o comando QVBoxLayout.
     #adicionar layout para organizar os elementos
+    
     Layout = QVboxLayout()
-
+    # para adicionar a label_nome e edit_nome ao
+    # organizador (layout) vertical, usamos o comando
+    # add(adicionar)Widget(controle). assim os controles
+    # irão aparecer um embaixo do outro, pois este organizado
+    # é do tipo vartical
+    
     layout.addwigdet(self.label_nome)
     layout.addWigdet(self.edit_nome)
 
